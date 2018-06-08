fd470d57767b02e9880383c679445e0888b787bb
bessemouelhi <bessem.mouelhi@outlook.fr>
2018-06-09 01:05:10 +0200

    add tablayout

--- app/src/main/java/com/appdevloop/viewpagerapp/PageAdapter.java
+++ app/src/main/java/com/appdevloop/viewpagerapp/PageAdapter.java
@@ -1,7 +1,39 @@
 package com.appdevloop.viewpagerapp;
 
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.app.FragmentStatePagerAdapter;
+
 /**
  * Created by AppDevloop on 09/06/2018.
  */
-public class PageAdapter {
+public class PageAdapter extends FragmentStatePagerAdapter {
+
+    // 1 - Array of colors that will be passed to PageFragment
+    private int[] colors;
+
+    // 2 - Default Constructor
+    public PageAdapter(FragmentManager mgr, int[] colors) {
+        super(mgr);
+        this.colors = colors;
+    }
+
+    @Override
+    public int getCount() {
+        return colors.length; // 3 - Number of page to show
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        // 4 - Page to return
+        return(PageFragment.newInstance(position, this.colors[position]));
+    }
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return "Page "+position;
+    }
 }
--- app/src/main/java/com/appdevloop/viewpagerapp/PageFragment.java
+++ app/src/main/java/com/appdevloop/viewpagerapp/PageFragment.java
@@ -3,9 +3,12 @@ package com.appdevloop.viewpagerapp;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 
 /**
@@ -14,52 +17,52 @@ import android.view.ViewGroup;
  * create an instance of this fragment.
  */
 public class PageFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    // 1 - Create keys for our Bundle
+    private static final String KEY_POSITION="position";
+    private static final String KEY_COLOR="color";
 
 
-    public PageFragment() {
-        // Required empty public constructor
-    }
+    public PageFragment() { }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment PageFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static PageFragment newInstance(String param1, String param2) {
-        PageFragment fragment = new PageFragment();
+
+    // 2 - Method that will create a new instance of PageFragment, and add data to its bundle.
+    public static PageFragment newInstance(int position, int color) {
+
+        // 2.1 Create new fragment
+        PageFragment frag = new PageFragment();
+
+        // 2.2 Create bundle and add it some data
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
+        args.putInt(KEY_POSITION, position);
+        args.putInt(KEY_COLOR, color);
+        frag.setArguments(args);
+
+        return(frag);
     }
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_page, container, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        // 3 - Get layout of PageFragment
+        View result = inflater.inflate(R.layout.fragment_page, container, false);
+
+        // 4 - Get widgets from layout and serialise it
+        LinearLayout rootView= (LinearLayout) result.findViewById(R.id.fragment_page_rootview);
+        TextView textView= (TextView) result.findViewById(R.id.fragment_page_title);
+
+        // 5 - Get data from Bundle (created in method newInstance)
+        int position = getArguments().getInt(KEY_POSITION, -1);
+        int color = getArguments().getInt(KEY_COLOR, -1);
+
+        // 6 - Update widgets with it
+        rootView.setBackgroundColor(color);
+        textView.setText("Page numéro "+position);
+
+        Log.e(getClass().getSimpleName(), "onCreateView called for fragment number "+position);
+
+        return result;
     }
 
 }
